49d768c542eef32cae0ed53fcd623ea697233e06
gameflix2 <oarsenaldegames@gmail.com>
2026-03-04 12:20:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ loginForm.addEventListener('submit', function(e) {
 
   // DEFINA AQUI O EMAIL E SENHA QUE VOCÊ QUER
   const emailCorreto = "testegratis@gameflix.com";
-  const senhaCorreta = "thiarlesgay";
+  const senhaCorreta = "a";
 
   if (emailInput === emailCorreto && passwordInput === senhaCorreta) {
     // Esconde a tela de login
@@ -196,4 +196,5 @@ window.addEventListener('click', function(event) {
     if (event.target === modalWpp) closeWppModal();
     if (event.target === modalNetflix) closeModal();
 });
+
 
